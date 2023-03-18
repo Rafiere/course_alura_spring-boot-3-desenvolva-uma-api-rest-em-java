c69b8000215c055380b2808564618564ee97c179
Rafiere <rafapgbr@gmail.com>
2023-03-18 13:33:50 -0300

    feat: add initial project files

---
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package med.voll.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	/* Essa é a classe que utilizamos para executar o projeto. */
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
