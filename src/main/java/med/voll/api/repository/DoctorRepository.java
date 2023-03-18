e62a2619df35970807f9f97222ea3368aac714af
Rafiere <rafapgbr@gmail.com>
2023-03-18 19:43:35 -0300

    feat: add query to list "GetDoctorResponse".

--- src/main/java/med/voll/api/repository/DoctorRepository.java
+++ src/main/java/med/voll/api/repository/DoctorRepository.java
@@ -1,8 +1,15 @@
 package med.voll.api.repository;
 
 import med.voll.api.entity.Doctor;
+import med.voll.api.response.GetDoctorResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DoctorRepository extends JpaRepository<Doctor, String> {
 
+	@Query("SELECT new med.voll.api.response.GetDoctorResponse( " +
+	       "d.name, d.email, d.crm, d.speciality) FROM Doctor d ")
+	Page<GetDoctorResponse> getDoctors(Pageable pageable);
 }
