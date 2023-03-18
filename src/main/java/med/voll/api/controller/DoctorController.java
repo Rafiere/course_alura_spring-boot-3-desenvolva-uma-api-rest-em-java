018751aa77ecb47e62b8464e36f28b4c402fc5f8
Rafiere <rafapgbr@gmail.com>
2023-03-18 19:43:20 -0300

    feat: add pagination

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,14 +1,17 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import med.voll.api.entity.Address;
 import med.voll.api.entity.Doctor;
 import med.voll.api.repository.DoctorRepository;
 import med.voll.api.request.RegisterADoctorRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.response.GetDoctorResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/doctors")
@@ -18,7 +21,9 @@ public class DoctorController {
 	private final DoctorRepository repository;
 
 	@PostMapping
-	public String registerADoctor(@RequestBody RegisterADoctorRequest request) {
+	@Transactional //Precisamos ter uma transação ativa para realizarmos a escrita.
+	//O "@Valid" diz que o Spring precisa validar o DTO com o Bean Validation.
+	public String registerADoctor(@RequestBody @Valid RegisterADoctorRequest request) {
 
 		Address address = Address.of(request.addressData()
 		                                    .logradouro(),
@@ -35,9 +40,15 @@ public class DoctorController {
 		                             request.addressData()
 		                                    .uf());
 
-		Doctor doctor = Doctor.of(request.name(), request.email(), request.crm(), request.speciality(), address);
+		Doctor doctor = Doctor.of(request.name(), request.email(), request.crm(), request.telefone(), request.speciality(), address);
 
 		repository.save(doctor);
 		return "";
+	}
+
+	@GetMapping
+	public Page<GetDoctorResponse> listDoctors(Pageable pagination){
+
+		return repository.getDoctors(pagination);
 	}
 }
