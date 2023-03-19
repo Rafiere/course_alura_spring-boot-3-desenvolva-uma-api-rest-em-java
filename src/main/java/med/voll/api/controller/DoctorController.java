f39b7a21cc857d999cd37cc39b45f0ec248e2c71
Rafiere <rafapgbr@gmail.com>
2023-03-18 22:40:53 -0300

    feat: add update and delete a doctor method.

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -6,9 +6,11 @@ import med.voll.api.entity.Address;
 import med.voll.api.entity.Doctor;
 import med.voll.api.repository.DoctorRepository;
 import med.voll.api.request.RegisterADoctorRequest;
+import med.voll.api.request.UpdateADoctorRequest;
 import med.voll.api.response.GetDoctorResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +25,7 @@ public class DoctorController {
 	@PostMapping
 	@Transactional //Precisamos ter uma transação ativa para realizarmos a escrita.
 	//O "@Valid" diz que o Spring precisa validar o DTO com o Bean Validation.
-	public String registerADoctor(@RequestBody @Valid RegisterADoctorRequest request) {
+	public Void registerADoctor(@RequestBody @Valid RegisterADoctorRequest request) {
 
 		Address address = Address.of(request.addressData()
 		                                    .logradouro(),
@@ -43,12 +45,35 @@ public class DoctorController {
 		Doctor doctor = Doctor.of(request.name(), request.email(), request.crm(), request.telefone(), request.speciality(), address);
 
 		repository.save(doctor);
-		return "";
+		return null;
 	}
 
+	/* Se o parâmetro "size" não for passado na URL, teremos o "size" igual a "4" registros. */
 	@GetMapping
-	public Page<GetDoctorResponse> listDoctors(Pageable pagination){
+	public Page<GetDoctorResponse> listDoctors(@PageableDefault(size = 4) Pageable pagination){
 
 		return repository.getDoctors(pagination);
+	}
+
+	@PutMapping
+	@Transactional //Como estamos dentro de uma transação, na JPA, se carregamos a entidade e mudamos
+	//um atributo, quando a transação for commitada, ela entenderá que o objeto foi alterado e o salvará
+	//automaticamente, assim, não precisamos usar o método "save()".
+	public Void updateADoctor(@RequestBody @Valid UpdateADoctorRequest request){
+
+		Doctor doctor = repository.getReferenceById(request.id());
+
+		doctor.update(request.name(), request.telefone(), request.addressData());
+
+		return null;
+	}
+
+	@DeleteMapping("{doctorId}")
+	@Transactional
+	public Void deleteADoctor(@PathVariable String doctorId){
+
+		repository.deleteById(doctorId);
+
+		return null;
 	}
 }
