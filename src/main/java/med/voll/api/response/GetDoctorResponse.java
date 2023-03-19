a16d7fd8abc4968d4677d1dc899e8ace3fcb6288
Rafiere <rafapgbr@gmail.com>
2023-03-18 22:40:17 -0300

    refactor! refactoring response

--- src/main/java/med/voll/api/response/GetDoctorResponse.java
+++ src/main/java/med/voll/api/response/GetDoctorResponse.java
@@ -2,6 +2,6 @@ package med.voll.api.response;
 
 import med.voll.api.enums.Speciality;
 
-public record GetDoctorResponse(String name, String email, String crm, Speciality speciality) {
+public record GetDoctorResponse(String id, String name, String email, String crm, Speciality speciality) {
 
 }
